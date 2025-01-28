14d3d5fe4243c3c08b2046b9378285b814ec3aa5
Dhruv <dhruvyadav2905@gmail.com>
2025-01-27 19:48:45 -0500

    assignment1: knockknock

---
+++ Assignments/Assignment1/Assignment1/Assignment1/Controllers/KnockKnockController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment1.Controllers
+{
+    [Route("api/q4/")]
+    [ApiController]
+    public class KnockKnockController : ControllerBase
+    {
+        [HttpGet(template: "knockknock")]
+        public string getKnockKnock()
+        {
+            return "Who's there?";
+        }
+    }
+}
--- Assignments/Assignment1/Assignment1/Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
+++ Assignments/Assignment1/Assignment1/Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bea86e625a806cf2c552fd9b9e3975cda6a6c0a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
