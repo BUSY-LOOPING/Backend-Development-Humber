18a5a85c2174929396d0afcbc62f1483100dacc4
Dhruv <dhruvyadav2905@gmail.com>
2025-02-20 18:26:07 -0500

    assign2: ques1, ques3

---
+++ Assignments/Assignment2/Assignment2/Controllers/Question1Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment2.Controllers
+{
+    [Route("api/J1")]
+    [ApiController]
+    public class Question1Controller : ControllerBase
+    {
+        [HttpPost(template: "Delivedroid")]
+        [Consumes("application/x-www-form-urlencoded")]
+        public int FinalScore([FromForm] int Collisions, [FromForm] int Deliveries)
+        {
+            int score = (Deliveries * 50) + (Collisions * -10);
+            score = Deliveries > Collisions ? score + 500: score;
+            return score;
+        }
+    }
+}
